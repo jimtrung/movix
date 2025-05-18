925321c658dadcf8eee1b4c91ff459ccf80ba30f
jimtrung <nguyenhaitrung737@gmail.com>
2025-05-18 21:24:05 +0700

    feat: sos

--- scripts/general.js
+++ scripts/general.js
@@ -7,33 +7,23 @@ if (localStorage.getItem('dark-mode') === "enabled") {
 }
 
 document.querySelector('footer').innerHTML = `
-  <hr class="hr"> 
-  <div class="footer-content"> 
-    <ul>
-      <h3>Về Movix</h3>
-      <li onclick="window.location.href='about.html'">Giới thiệu</li>
-      <li>Các gói dịch vụ</li>
-      <li>Liên hệ quảng cáo</li>
-      <li>Trung tâm hỗ trợ</li>
+  <footer class="footer">
+    <div class="social">
+      <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+      <a href="#"><i class="fa-brands fa-github"></i></a>
+      <a href="https://www.facebook.com/share/1DTPEeZQHG/?mibextid=wwXIfr"><i class="fa-brands fa-facebook-f"></i></a>
+      <a><i class="fa-brands fa-youtube"></i></a>
+    </div>
+    <ul class="list">
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Service</a></li>
+      <li><a href="about.html">About Us</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Testimonials</a></li>
     </ul>
-    <ul> 
-      <h3>Quy định</h3>
-      <li>Điều khoản sử dụng</li>
-      <li>Chính sách thanh toán</li> 
-      <li>Chính sách bảo mật thông tin</li>
-      <li>Chính sách bảo hành</li>
-    </ul>
-    <ul> 
-      <h3>Liên Hệ</h3>
-      <li><i class="fa-solid fa-phone"></i><span class="sdt">19008386</span></li>
-      <li><i class="fa-solid fa-envelope"></i><span class="mail">movix@gmail.com</span></li>
-      <p class="yap">Theo dõi chúng tôi qua:</p>
-      <ul class="socials">
-        <li><a class="fb" href="https://www.facebook.com/share/1DTPEeZQHG/?mibextid=wwXIfr" 
-        target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
-        <li><a class="ytb" href="#"><i class="fa-brands fa-youtube"></i></a></li>
-        <li><a class="toptop" href="#"><i class="fa-brands fa-tiktok"></i></a></li>
-      </ul>
-    </ul>
-  </div>
+
+    <p class="copyright">
+      © Movix | All Rights Reserved
+    </p>
+  </footer>
 `
